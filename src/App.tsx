5a83d1ce9110e0ed7327a426cccc0654c8a9a800
Qri <2894700792@qq.com>
2022-01-10 09:34:33 +0800

    代码清理

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,8 @@
 import React from 'react';
 import './App.css';
-import BubblePanel from "./room/blive/view/BubblePanel";
-import BLiveMsgSource from "./room/blive/msgsource/BLiveMsgSource";
 import BliveBubbleRoom from "./room/blive/BliveBubbleRoom";
 
-
-// class MockMsgSource implements MsgSource {
-{/*    onMsgs: Array<(normalMsg: NormalMsg) => void>*/}
-
-//     registerOnMsg(onMsg: (normalMsg: NormalMsg) => void): void {
-//         this.onMsgs.push(onMsg)
-//     }
-//
-//     constructor() {
-//         this.onMsgs = []
-//         for (let i = 0; i < 10; i++) {
-//             cloudarTimer.registerEvent({
-//                 shouldExecuteMillSeconds: new Date().getTime() + 1000 * i,
-//                 event: () => {
-//                     for (let onMsg of this.onMsgs) {
-//                         onMsg({
-//                             uniqueId: "12345" + i,
-//                             sender: {
-//                                 avatar: "http://pic.ggemo.com/picgo/qriheadimg.jpg",
-//                                 name: "qri",
-//                                 uniqueId: "qri#23330"
-//                             },
-//                             content: `草草草草草草草草草草草草草草草草草草草草草草草草草草草草草草草草草草草草草草草草`,
-//                             sendTime: new Date().getTime(),
-//                             reply2UniqueId: undefined,
-//                             extraInfo: undefined
-//                         })
-//                     }
-//                 },
-//                 isExecuting: false,
-//                 isFinished: false
-//             })
-//         }
-//
-//     }
-// }
-
 function App() {
-    // const mockMsgSource = new BLiveMsgSource(336119);
     return (
         <BliveBubbleRoom roomId={336119} />
     );
--- src/room/blive/msgsource/BLiveMsgSource.ts
+++ src/room/blive/msgsource/BLiveMsgSource.ts
@@ -1,7 +1,5 @@
 import DMclientRE from "./blive/dm_client_re";
-import React from "react";
 import {GiftStatic} from "./bubbleblivetypes/BubbleBliveCommon";
-import {BliveBubbleSendGiftMsg} from "./bubbleblivetypes/BliveBubbleMsg";
 import {BaseBliveMsg} from "./bubbleblivetypes/BliveMsg";
 
 export default class BLiveMsgSource {
@@ -16,22 +14,6 @@ export default class BLiveMsgSource {
             console.log("ALL_MSG", data)
             this.emitMsg(data);
         })
-        // client.on("INTERACT_WORD", (d) => {
-        //     let normalMsg: NormalMsg = {
-        //         uniqueId : `bili-enterroom-${d.data.uid}`,
-        //         sender: {
-        //             uniqueId: `bili-${d.data.uid}`,
-        //             name: d.data.uname,
-        //             avatar: "http://pic.ggemo.com/picgo/qriheadimg.jpg"
-        //         },
-        //         content: React.createElement("h3", {}, "欢迎" + d.data.uname + " 进入直播间~"),
-        //         sendTime: d.data.trigger_time/1000000,
-        //         level: Level.NORMAL,
-        //         extraInfo: {raw: d}
-        //     }
-        //     this.emitMsg(normalMsg)
-        // })
-        // //
         // client.on("ENTRY_EFFECT", d => {
         //     d = {
         //         "cmd": "ENTRY_EFFECT",
@@ -80,48 +62,6 @@ export default class BLiveMsgSource {
             //     }
             // }
             // this.emitMsg(normalMsg)
-        // })
-        //
-        // client.on("DANMU_MSG", async (d) => {
-        //
-        //     const userId = d.info[2][0];
-        //     let headImg = await this.getUserHeadImg(userId);
-        //     let normalMsg: NormalMsg = {
-        //         uniqueId : `bili-DANMU_MSG-${d.info[0][4]}-${d.info[0][5]}`,
-        //         sender: {
-        //             uniqueId: `bili-${d.info[2][0]}`,
-        //             name: d.info[2][1],
-        //             avatar: headImg
-        //         },
-        //         content: d.info[1],
-        //         sendTime: d.info[0][4],
-        //         level: Level.NORMAL,
-        //         extraInfo: {
-        //             raw: d
-        //         }
-        //     }
-        //     this.emitMsg(normalMsg)
-        // })
-        //
-        // client.on("SEND_GIFT", async d => {
-        //     let innerSendGift: InnerSendGift = {
-        //         raw: d,
-        //         giftStatic: this.giftStaticInfos.get(d.data.giftId)
-        //     }
-        //     let normalMsg: NormalMsg = {
-        //         uniqueId : `bili-SEND_GIFT-${d.data.timestamp}-${d.data.rnd}`,
-        //         sender: {
-        //             uniqueId: `bili-${d.data.uid}`,
-        //             name: d.data.uname,
-        //             avatar: d.data.face
-        //         },
-        //         content: React.createElement("h2", {}, `${d.data.uname} 送出 ${d.data.giftName} * ${d.data.num}`,
-        //             React.createElement("img", {src: this.giftStaticInfos.get(d.data.giftId)?.gif})),
-        //         sendTime: d.data.timestamp,
-        //         level: Level.NORMAL,
-        //         extraInfo: innerSendGift
-        //     }
-        //     this.emitMsg(normalMsg)
         // })
         client.Connect()
     }
