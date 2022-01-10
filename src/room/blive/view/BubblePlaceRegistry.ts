7b9bde78fb88a78de8d34c161828e3a5eb2234c1
sunqihong <sunqihong@baidu.com>
2022-01-10 21:57:33 +0800

    placeRegistry 接口注释

--- src/room/blive/view/BubblePlaceRegistry.ts
+++ src/room/blive/view/BubblePlaceRegistry.ts
@@ -3,18 +3,32 @@ import Place, {Position, Scale} from "./BubblePlace";
 export default class BubblePlaceRegistry {
     scale: Scale = {width: 0, height: 0};
 
+    /**
+     * 设置整个面板的大小. 窗口大小变动的时候调用这个方法
+     * @param scale
+     */
     setScale(scale: Scale) {
         this.scale = scale
     }
 
-    registerUsePlace(place: Place) {
-
-    }
-
+    /**
+     * 输入气泡占用的scale, 获取将要放置的位置
+     * @param scale
+     */
     getPosition(scale: Scale): Position {
         return {
             x: 100 * Math.random() * (this.scale.width - scale.width) / this.scale.width,
             y: 100 * Math.random() * (this.scale.height - scale.height) / this.scale.height
         }
     }
+
+    /**
+     * 放置气泡之后, 向本组件注册已使用该块区域
+     * @param place
+     */
+    registerUsePlace(place: Place) {
+
+    }
 }
+
+
