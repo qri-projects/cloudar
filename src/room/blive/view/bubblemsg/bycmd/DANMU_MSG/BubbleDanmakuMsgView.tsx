81a87739a268599de33070c43df2bc11494450fb
Qri <2894700792@qq.com>
2022-01-14 00:50:59 +0800

    小表情

--- src/room/blive/view/bubblemsg/bycmd/DANMU_MSG/BubbleDanmakuMsgView.tsx
+++ src/room/blive/view/bubblemsg/bycmd/DANMU_MSG/BubbleDanmakuMsgView.tsx
@@ -5,6 +5,16 @@ import BubbleMsgHolderView from "../../bubblemsgholder/BubbleMsgHolderView";
 
 export default class BubbleDanmakuMsgView extends BubbleViewByCmd<BliveBubbleDanmuMsg, any>{
     render() {
+        let emotionInfo = this.props.msg.raw.info[0][13];
+        // @ts-ignore
+        if (emotionInfo && emotionInfo !== "{}") {
+            return <img src={emotionInfo["url"]}
+                        style={{
+                            width: emotionInfo.width / 2, height: emotionInfo.height / 2,
+                            margin: "0px -8px -6px -8px"
+                        }}
+                        alt={this.props.msg.raw.info[1]}/>
+        }
         return this.props.msg.raw.info[1]
         // return this.props.msg.raw.info.content;
     }
